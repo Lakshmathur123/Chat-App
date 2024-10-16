d4f5da0c03ef9a83971d90fba51748dd199b35cf
Lakshmathur123 <lakshmathur2004@gmail.com>
2024-10-16 13:21:59 +0530

    add

--- Client/src/Components/Search.jsx
+++ Client/src/Components/Search.jsx
@@ -9,14 +9,6 @@ const Search = () => {
       <input type="text" placeholder='Search here..' />
       <a href=""> <FontAwesomeIcon icon={faSearch} className='searchIcon' /></a>
       </div>
-      <div className='userChat'>
-        <img src="/images/user person.png" alt="user" />
-        <div className='userChatInfo'>
-          <span>Laksh</span>
-         <p>Hello</p>
-        </div>
-        <p className='lastOnline'>10:00 AM</p>
-      </div>
     </div>
   )
 }
